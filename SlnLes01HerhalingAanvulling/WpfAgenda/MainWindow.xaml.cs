039518bb4d4a03094f8f58d207b20d0db976a19a
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-02-08 15:26:30 +0100

    Oef3 Layout

---
+++ SlnLes01HerhalingAanvulling/WpfAgenda/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAgenda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Error = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lblToepassen_Click(object sender, RoutedEventArgs e)
+        {
+            CheckForErrors();
+            AfspraakAanmaken();
+            TonenAfspraken();
+        }
+        private void CheckForErrors()
+        {
+            if (txbTitel.Text == "")
+            {
+                Error = true;
+            }
+            if (DPDatum.Text == "")
+            {
+
+            }
+            if (CBType.SelectedIndex == -1)
+            {
+
+            }
+
+            MessageBox.Show(RBGeen.IsChecked.ToString());
+        }
+        private void AfspraakAanmaken()
+        {
+
+        }
+        private void TonenAfspraken()
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
--- SlnLes01HerhalingAanvulling/WpfOxo/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ SlnLes01HerhalingAanvulling/WpfOxo/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace WpfOxo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfOxo;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfOxo;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
