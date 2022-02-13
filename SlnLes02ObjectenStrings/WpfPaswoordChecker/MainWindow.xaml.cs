33ca12ba2bcc2c23b725fcf49e10a4170220d617
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-02-13 11:25:27 +0100

    Wachtwoordchecker

---
+++ SlnLes02ObjectenStrings/WpfPaswoordChecker/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPaswoordChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txbPasswoord.Text = "";
+        }
+        private void checkPassword(string p)
+        {
+            string substring = "";
+            byte ASCII = 0;
+            bool[] errors = { true, true, true, true, true };
+            int err = 5;
+            for (int i = 0; i < p.Length; i++)
+            {
+                substring = (p.Substring(i, 1));
+                char subchar = char.Parse(substring);
+                int ascii = (int)subchar;
+                if (ascii > 96 && ascii <= 122)
+                {
+                    errors[0] = false;
+                }
+                if (ascii > 64 && ascii <= 90)
+                {
+                    errors[1] = false;
+                }
+                if (p.Length >= 8)
+                {
+                    errors[2] = false;
+                }
+                if (ascii > 47 && ascii <= 57)
+                {
+                    errors[3] = false;
+                }
+                if (ascii < 48 || ascii > 57 && ascii <= 64 || ascii > 90 && ascii <= 96 || ascii == 126)
+                {
+                    errors[4] = false;
+                }
+            }
+            byte R = Convert.ToByte(0);
+            byte G = Convert.ToByte(255);
+            byte B = Convert.ToByte(0);
+            if (!errors[2])
+            {
+                lblAchtKarakters.Foreground = new SolidColorBrush(Color.FromRgb(R, G, B));
+                err--;
+            }
+            if (!errors[0])
+            {
+                lblKleineletter.Foreground = new SolidColorBrush(Color.FromRgb(R, G, B));
+                err--;
+            }
+            if (!errors[1])
+            {
+                lblHoofdletter.Foreground = new SolidColorBrush(Color.FromRgb(R, G, B));
+                err--;
+            }
+            if (!errors[3])
+            {
+                lblEenCijfer.Foreground = new SolidColorBrush(Color.FromRgb(R, G, B));
+                err--;
+            }
+            if (!errors[4])
+            {
+                lblVreemdKarakter.Foreground = new SolidColorBrush(Color.FromRgb(R, G, B));
+                err--;
+            }
+            if (err == 0) btnRegistreer.IsEnabled = true;
+            else btnRegistreer.IsEnabled = false;
+        }
+
+        private void txbPasswoord_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string passwoord = txbPasswoord.Text;
+            checkPassword(passwoord);
+        }
+    }
+}
