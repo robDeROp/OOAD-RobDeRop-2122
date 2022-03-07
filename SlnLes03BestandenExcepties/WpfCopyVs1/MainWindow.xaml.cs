d9a2809b45d88c616188741721ec8e9c41fdc5ad
robDeROp <79730799+robDeROp@users.noreply.github.com>
2022-03-07 12:04:37 +0100

    Taak1 en 2

---
+++ SlnLes03BestandenExcepties/WpfCopyVs1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace WpfCopyVs1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            string FileIn = txbFileIn.Text;
+            string FileOut = txbFileOut.Text;
+            string DefaultPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            string FolderName = "Files";
+            string fullINPath = System.IO.Path.Combine(DefaultPath, FolderName, FileIn);
+            string[] DocumentContext = {""};
+            try
+            {
+                DocumentContext = File.ReadAllLines(fullINPath);
+                lblMessage.Content = "Het bestand is overgezet";
+            }
+            catch (Exception)
+            {
+                lblMessage.Content = "Error bij het lezen van het bestand: " + FileIn;
+            }
+            string fullOUTPath = System.IO.Path.Combine(DefaultPath, FolderName, FileOut);
+            try
+            {
+                File.WriteAllLines(fullOUTPath, DocumentContext); //Not authorized, ik vind niet waarom.
+                lblMessage.Content = "Het bestand is overgezet";
+            }
+            catch (Exception)
+            {
+                lblMessage.Content = "Error bij het schrijven naar het bestand: " + FileOut;
+            }
+        }
+    }
+}
---
+++ SlnLes03BestandenExcepties/WpfCopyVs2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WpfCopyVs2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string pathIn = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog dialog = new OpenFileDialog();
+        SaveFileDialog SDialog = new SaveFileDialog();
+        private void btnKIES_Click(object sender, RoutedEventArgs e)
+        {
+            dialog.ShowDialog();
+            pathIn = dialog.FileName;
+            txbPath.Text = pathIn;
+            if (pathIn != "") btnGO.IsEnabled = true;
+            else btnGO.IsEnabled = false;
+        }
+
+        private void btnGO_Click(object sender, RoutedEventArgs e)
+        {
+            SDialog.ShowDialog();
+            string[] ContentF1 = File.ReadAllLines(pathIn);
+            try
+            {
+                File.WriteAllLines(SDialog.FileName, ContentF1);
+                lblMessage.Content = "Bestand overgezet";
+            }
+            catch (Exception)
+            {
+                lblMessage.Content = "Error";
+            }
+        }
+    }
+}
